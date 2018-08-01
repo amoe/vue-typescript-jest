7c7d9c812089dac488121d7d423ac318ca873a8b
David Banks <amoebae@gmail.com>
2018-08-01 19:31:24 +0100

    rename root component

--- src/entry.ts
+++ src/entry.ts
@@ -1,8 +1,8 @@
 import Vue from "vue";
-import App from "./components/App.vue";
+import ApplicationRoot from "./components/ApplicationRoot.vue";
 
 const vm: Vue = new Vue({
     el: "#app",
     template: "<app/>",
-    components: { App }
+    components: { ApplicationRoot }
 });
