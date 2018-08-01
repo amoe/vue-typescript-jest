79034f5a9542d9ecb4e4c248d68f3a10f341206c
David Banks <amoebae@gmail.com>
2018-08-01 20:03:48 +0100

    conform webpack conf

--- webpack.config.js
+++ webpack.config.js
@@ -9,19 +9,10 @@ module.exports = {
         filename: "bundle.js"
     },
     module: {
-        rules: [{
+        rules: [
+            {
                 test: /\.vue$/,
-                loader: 'vue-loader',
-                options: {
-                    loaders: {
-                        // Since sass-loader (weirdly) has SCSS as its default parse mode, we map
-                        // the "scss" and "sass" values for the lang attribute to the right configs here.
-                        // other preprocessors should work out of the box, no loader config like this necessary.
-                        'scss': 'vue-style-loader!css-loader!sass-loader',
-                        'sass': 'vue-style-loader!css-loader!sass-loader?indentedSyntax',
-                    }
-                    // other vue-loader options go here
-                }
+                loader: 'vue-loader'
             },
             {
                 test: /\.tsx?$/,
@@ -29,13 +20,6 @@ module.exports = {
                 exclude: /node_modules/,
                 options: {
                     appendTsSuffixTo: [/\.vue$/],
-                }
-            },
-            {
-                test: /\.(png|jpg|gif|svg)$/,
-                loader: 'file-loader',
-                options: {
-                    name: '[name].[ext]?[hash]'
                 }
             },
             {
