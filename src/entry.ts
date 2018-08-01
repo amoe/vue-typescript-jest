034d103499cabbf7e52d2b8d51f3a82a677392a9
David Banks <amoebae@gmail.com>
2018-08-01 19:50:54 +0100

    migrate more

--- src/entry.ts
+++ src/entry.ts
@@ -1,6 +1,29 @@
 import Vue from "vue";
 import ApplicationRoot from "./components/ApplicationRoot.vue";
 import store from './vuex/store';
+import mymodule from './mymodule';
+import * as log from 'loglevel';
+
+log.setLevel('debug');
+
+function getRenderingMode() {
+    let result: string;
+
+    if (document.compatMode === 'CSS1Compat') {
+        result = "STANDARDS_COMPLIANCE";
+    } else {
+        result = "QUIRKS";
+    }
+
+    return result;
+}
+
+document.addEventListener("DOMContentLoaded", e => {
+    console.log("Rendering mode: %o", getRenderingMode());
+    console.log("The answer is: %o", mymodule.meaningOfLife());
+
+});
+
 
 document.addEventListener("DOMContentLoaded", e => {
     const vueInstance = new Vue({
---
+++ src/mymodule.ts
@@ -0,0 +1,6 @@
+const publicExports = {
+    meaningOfLife: function() { return 42; }
+};
+
+export default publicExports;
+
